ba1f5a28ee7ed4a2293147a80ae98747eb065e1c
Kirill Semibratov <KSemibratoFF@gmail.com>
2024-05-11 23:19:02 +0300

    I wrote the solution code task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] initialArray = { "Hello", "2", "world", ":-)" };
+        // string[] initialArray = { "1234", "1567", "-2", "computer science" };
+        // string[] initialArray = { "Russia", "Denmark", "Kazan" };
+        string[] shortStrings = FilterShortStrings(initialArray);
+        Console.WriteLine(string.Join(", ", shortStrings)); // Выводит результат
+    }
+
+    static string[] FilterShortStrings(string[] inputStrings)
+    {
+        string[] result = new string[0];
+        foreach (var str in inputStrings)
+        {
+            if (str.Length <= 3)
+            {
+                string[] tempResult = new string[result.Length + 1];
+                Array.Copy(result, tempResult, result.Length);
+                tempResult[result.Length] = str;
+                result = tempResult;
+            }
+        }
+        return result;
+    }
+}
+
